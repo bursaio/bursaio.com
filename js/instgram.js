a96a48764bf47edaf7360ff925c38baf7dbf39f1
Fuat POYRAZ <fuat@fuatpoyraz.com>
2018-05-02 21:33:11 +0300

    extra stiller eklendi

--- js/instgram.js
+++ js/instgram.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
         tagName: 'bursaio',
         clientId: '490053ebc98f4867910b2bb0f0542fac',
         accessToken: '6175788897.ba4c844.bf9307fc80534c1a826c65b5d82bc2bd',
+        template: '<li><a href="{{link}}" target="_blank"><img src="{{image}}" /></a></li>'
     });
     userFeed.run();
 });
