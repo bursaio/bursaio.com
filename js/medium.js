931d4eadb65c3d8964cea83ed4e664d4ac5e4c56
ylmazbarscan <ylmazbarscan@outlook.com>
2018-05-02 23:10:27 +0300

    gereksiz marginler kaldırıldı

--- js/medium.js
+++ js/medium.js
@@ -8,9 +8,8 @@ $(function () {
 			var output = '';
 			$.each(response.items, function (k, item) {
 				
-				output += '<div class="col-md-12"><div class="blog-post">';
-				output += '<div class="blog-content"><h4><a href="'+ item.link + '">' + item.title + '</a></h4>';
-				output += '<div class="post-meta"><span>By ' + item.author + '</span></div>';
+				output += '<div class="col-md-12"><div class="blog-post" style="text-align:left">';
+				output += '<div class="blog-content"><h4><a href="'+ item.link + '">' + item.title + '</a></h4><span>By ' + item.author + '</span>';
 				output += '</div></div></div>';
 				return k ;
 			});
