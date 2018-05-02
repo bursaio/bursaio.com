15bf3f123580d89a456cf944d6c7f35c57ff7bd6
Fuat POYRAZ <fuat@fuatpoyraz.com>
2018-05-02 20:48:04 +0300

    instafeed eklendi

--- js/instgram.js
+++ js/instgram.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-	console.log('instgram');
+	
+	var userFeed = new Instafeed({
+        get: 'tagged',
+        tagName: 'bursaio',
+        clientId: '490053ebc98f4867910b2bb0f0542fac',
+        accessToken: '6175788897.ba4c844.bf9307fc80534c1a826c65b5d82bc2bd',
+    });
+    userFeed.run();
 });
